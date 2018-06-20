14b676ef71fa28cccbc4b656ee8c85f1e0ff0951
abs <donato.dicapua@pleiade.it>
2018-06-20 17:38:50 +0200

    Aggiunto progetto nuovo frontend

--- quick-poll/src/main/java/com/apress/domain/Option.java
+++ quick-poll/src/main/java/com/apress/domain/Option.java
@@ -1,8 +1,11 @@
 package com.apress.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,4 +22,6 @@ private Long id;
 @Column(name="OPTION_VALUE")
 private String value;
 // Getters and Setters omitted for brevity
+
 }
+
