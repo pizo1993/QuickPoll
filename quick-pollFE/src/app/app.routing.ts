d56aeb97d837c6ca20c018f98cde8d5726e28e83
pizo1993 <donato.dicapua-1993@outlook.it>
2018-07-15 13:19:46 +0200

    creata route per pagina dettaglio utente

--- quick-pollFE/src/app/app.routing.ts
+++ quick-pollFE/src/app/app.routing.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
 import { AuthGuard } from './guard/auth.guard';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'userDetails/:id', component: UserdetailsComponent, canActivate: [AuthGuard] },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
--- quick-pollFE/src/app/home/home.component.ts
+++ quick-pollFE/src/app/home/home.component.ts
@@ -9,17 +9,17 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
     currentUser: User;
     users: User[] = [];
-    showDetails: boolean[] = [];
-    
+    // Array di boolean, indice rappresentato dall'id di ciascun user, serve ad impostare per ciascun user la variabile booleana che abilita/disabilita la visualizzazione dei dettagli relativi all'utente rappresentato dalla chiave.
+    showInfo: Map<number, boolean>;
 
   constructor(public authenticationService: AuthenticationService, public router: Router, public userService: UserService) {
-    
+
   }
 
 
     ngOnInit() {
       this.currentUser = new User();
-      Object.assign(this.currentUser,JSON.parse(localStorage.getItem('currentUser')));
+      Object.assign(this.currentUser, JSON.parse(localStorage.getItem('currentUser')));
       this.loadAllUsersFullname();
     }
 /*
@@ -28,38 +28,39 @@ export class HomeComponent implements OnInit {
             this.loadAllUsers()
         });
     }
-*/  
-  
-    public changeShowDetails(id: number) {
-      return this.showDetails[id] =!this.showDetails[id];
+*/
+
+    public changeShowInfo(id: number) {
+      return this.showInfo[id] = !this.showInfo[id];
     }
-  
-    public detailsLabel(bool: boolean) {
-      return bool ? 'Nascondi Dettagli' : 'Visualizza Dettagli';
+
+    public infoLabel(bool: boolean) {
+      return bool ? 'Nascondi Info' : 'MostraInfo';
     }
-  
-  
-  
+
+// Chiamata al servizio userService. Viene creato un oggetto di tipo User per ogni istanza restituita dalla chiamata al servizio. 
     private loadAllUsersFullname() {
       this.userService.getAll()
         .then(u => {
-          let usersObject = u;   
+          const usersObject = u;
+          this.showInfo = new Map<number, boolean>();
           usersObject.forEach((user, i) => {
-          this.users[i]= new User();
-          Object.assign(this.users[i], user)     
-          this.showDetails[user.id]=false;
+          this.users[i] = new User();
+          Object.assign(this.users[i], user);
+          this.showInfo.set(this.users[i].id, false);
           });
+          
         });
     }
-  
-  
+
+
     logOut() {
-    console.log("Logout in corso")
+    console.log('Logout in corso');
     if (this.authenticationService.logOut()) {
           this.router.navigate(['/login']);
     } else {
-      console.log("ERRORE");
-    }     
+      console.log('ERRORE');
+    }
   }
-  
+
 }
